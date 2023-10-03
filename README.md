27f5bb2a4241db6c4f6376feeca402222ff9e420
Michal Hershkovitz <hershko.michal@gmail.com>
2023-10-03 23:27:00 +0300

    Added README

---
+++ README.md
@@ -0,0 +1,53 @@
+# Home assignment - CheckPoint - WordCount
+
+## Description
+WordCount app exposes API that receive for input a file name that located in s3 bucket, and 
+perform count of the words in the
+file and how many times each word appeared. 
+The app stores the results in another S3 bucket.
+
+```commandline
+POST /word-count
+payload: {fileName: <FILE-NAME>}
+headers: {
+            Content-Type: application/json;charset=UTF-8,
+            Authorization: <ACCESS-TOKEN>
+          }
+```
+
+## Assumptions
+- The provided file name must exist in the `cp-input-files` bucket
+- The file must be valid and contain text
+
+## Pre-requisite
+- Make sure you have installed Python3, Terraform, AWS CLI, and configured a `default` AWS CLI profile
+
+```bash
+terraform -help # prints Terraform options
+which aws # prints /usr/local/bin/aws
+aws --version # prints aws-cli/2.0.36 Python/3.7.4 Darwin/18.7.0 botocore/2.0.0
+aws configure # configure your AWS CLI profile
+python3 --version # prints Python 3 version
+```
+
+- locate `.env` file in your root directory
+
+```bash
+AWS_ACCESS_KEY = ********
+AWS_SECRET_KEY = ********
+S3_BUCKET_INPUT_FILES = 'cp-input-files'
+S3_BUCKET_OUTPUT_FILES = 'cp-output-files'
+ACCESS_TOKEN= *******
+APP_PORT=5000
+ENV=DEV
+```
+
+## Automation script
+- located in `scripts` directory
+- contains deployment into aws, API call to the deployed app and validation of the response
+- insert `GITHUB_TOKEN` in line 5
+- insert `ACCESS_TOKEN` in line 6
+- call using `sh scripts/deploy.sh`
+
+### NOTE
+I'm no longer afraid of Terraform, so thanks for that :)
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
+brew install jq
+
+GITHUB_TOKEN="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"
+ACCESS_TOKEN="aaa"
+
 cd ../deploy
 echo "Calling terraform init"
 terraform init
 echo "Starting deployment"
-terraform apply -input=false -auto-approve -var="github_token=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"
+terraform apply -input=false -auto-approve -var="github_token=${GITHUB_TOKEN}"
 
 HOSTNAME="$(terraform output -raw aws_lb_dns_name)"
 echo "Calling ${HOSTNAME}/count-words"
 echo
 
 PAYLOAD='{"fileName": "file1.txt"}'
-RESPONSE=`curl -s --request POST -H "Content-Type:application/json" -H "Authorization:aaa" ${HOSTNAME}/count-words --data "${PAYLOAD}"`
+RESPONSE=`curl -s --request POST -H "Content-Type:application/json" -H "Authorization:${ACCESS_TOKEN}" ${HOSTNAME}/count-words --data "${PAYLOAD}"` | jq
+# shellcheck disable=SC2006
+RESPONSE_MSG=`curl -s --request POST -H "Content-Type:application/json" -H "Authorization:${ACCESS_TOKEN}" ${HOSTNAME}/count-words --data "${PAYLOAD}"` | jq.message
 
 echo "RESPONSE: ${RESPONSE}"
-
-RESPONSE_MSG=$(\
-    curl -s --request POST -H "Content-Type:application/json" -H "Authorization:aaa" ${HOSTNAME}/count-words --data "${PAYLOAD}" \
-    | python -c "import sys, json; print json.load(sys.stdin)['data']['message']"\
-)
-
 echo "RESPONSE_MSG: ${RESPONSE_MSG}"
 
+terraform destroy -input=false -auto-approve -var="github_token=${GITHUB_TOKEN}"
+
+echo
+echo "*****************"
+echo "RESPONSE: ${RESPONSE}"
+echo "RESPONSE_MSG: ${RESPONSE_MSG}"
 if [ "RESPONSE_MSG" = "please find your output file in cp-output-files bucket" ]; then
     echo "SUCCESS"
 else
     echo "FAILURE"
 fi
+echo "*****************"
